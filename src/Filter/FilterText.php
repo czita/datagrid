789f41eed0920f0bd4e36917ecece4b854169bf8
Pavel Jirák <pavel.jirak@gmail.com>
2019-03-25 15:28:07 +0100

    filterText filters with quotation marks

--- src/Filter/FilterText.php
+++ src/Filter/FilterText.php
@@ -13,93 +13,112 @@ use Nette;
 class FilterText extends Filter
 {
 
-	/**
-	 * @var string
-	 */
-	protected $template = 'datagrid_filter_text.latte';
+    /**
+     * @var string
+     */
+    protected $template = 'datagrid_filter_text.latte';
 
-	/**
-	 * @var string
-	 */
-	protected $type = 'text';
+    /**
+     * @var string
+     */
+    protected $type = 'text';
 
-	/**
-	 * @var bool
-	 */
-	protected $exact = false;
+    /**
+     * @var bool
+     */
+    protected $exact = false;
 
-	/**
-	 * @var bool
-	 */
-	protected $split_words_search = true;
-
-
-	/**
-	 * Adds text field to filter form
-	 * @param Nette\Forms\Container $container
-	 */
-	public function addToFormContainer(Nette\Forms\Container $container)
-	{
-		$container->addText($this->key, $this->name);
-
-		$this->addAttributes($container[$this->key]);
-
-		if ($this->getPlaceholder()) {
-			$container[$this->key]->setAttribute('placeholder', $this->getPlaceholder());
-		}
-	}
+    /**
+     * @var bool
+     */
+    protected $split_words_search = true;
 
 
-	/**
-	 * Return array of conditions to put in result [column1 => value, column2 => value]
-	 * 	If more than one column exists in fitler text,
-	 * 	than there is OR clause put betweeen their conditions
-	 * Or callback in case of custom condition callback
-	 * @return array|callable
-	 */
-	public function getCondition()
-	{
-		return array_fill_keys($this->column, $this->getValue());
-	}
+    /**
+     * Adds text field to filter form
+     * @param Nette\Forms\Container $container
+     */
+    public function addToFormContainer(Nette\Forms\Container $container)
+    {
+        $container->addText($this->key, $this->name);
+
+        $this->addAttributes($container[$this->key]);
+
+        if ($this->getPlaceholder()) {
+            $container[$this->key]->setAttribute('placeholder', $this->getPlaceholder());
+        }
+    }
 
 
-	/**
-	 * @return boolean
-	 */
-	public function isExactSearch()
-	{
-		return $this->exact;
-	}
+    /**
+     * Return array of conditions to put in result [column1 => value, column2 => value]
+     *    If more than one column exists in fitler text,
+     *    than there is OR clause put betweeen their conditions
+     * Or callback in case of custom condition callback
+     * @return array|callable
+     */
+    public function getCondition()
+    {
+        return array_fill_keys($this->column, $this->getValue());
+    }
 
 
-	/**
-	 * @param boolean $exact
-	 * @return FilterText
-	 */
-	public function setExactSearch($exact = true)
-	{
-		$this->exact = $exact;
-		return $this;
-	}
+    /**
+     * Return array of searched words
+     * @param string $value
+     * @return array
+     */
+    public function getSplittedWords($value)
+    {
+        if ($this->hasSplitWordsSearch() === false) {
+            return [$value];
+        } elseif (strpos($value, '"') !== false) {
+            preg_match_all('/((?:\"[\w\s&]+\")|(\w+))+/', $value, $m);
+
+            return $m[0];
+        } else {
+            return explode(' ', $value);
+        }
+    }
 
 
-	/**
-	 * @param bool $split_words_search
-	 * @return FilterText
-	 */
-	public function setSplitWordsSearch($split_words_search)
-	{
-		$this->split_words_search = (bool) $split_words_search;
-
-		return $this;
-	}
+    /**
+     * @return boolean
+     */
+    public function isExactSearch()
+    {
+        return $this->exact;
+    }
 
 
-	/**
-	 * @return bool
-	 */
-	public function hasSplitWordsSearch()
-	{
-		return $this->split_words_search;
-	}
+    /**
+     * @param boolean $exact
+     * @return FilterText
+     */
+    public function setExactSearch($exact = true)
+    {
+        $this->exact = $exact;
+        return $this;
+    }
+
+
+    /**
+     * @param bool $split_words_search
+     * @return FilterText
+     */
+    public function setSplitWordsSearch($split_words_search)
+    {
+        $this->split_words_search = (bool)$split_words_search;
+
+        return $this;
+    }
+
+
+    /**
+     * @return bool
+     */
+    public function hasSplitWordsSearch()
+    {
+        return $this->split_words_search;
+    }
 }
